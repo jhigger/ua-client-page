0d97e18617080c8c8774941aae284d3bc1fb1bc6
jhigger <njohnjhigger@gmail.com>
2022-10-30 22:32:55 +0800

    text color

--- src/components/How.tsx
+++ src/components/How.tsx
@@ -50,19 +50,19 @@ const How = () => {
 const List = () => {
 	const list = [
 		{
-			color: "bg-orange-50 ring-orange-200",
+			color: "bg-orange-50 ring-orange-200 text-orange-500",
 			heading: "Connect your wallet and submit an application",
 			content:
 				"Log in via your wallet to submit an applications, once submitted you will get approved/denied within 24 hours. If there is an error in your answers we will ask you to resubmit the application with the correct information.",
 		},
 		{
-			color: "bg-green-50 ring-green-200",
+			color: "bg-green-50 ring-green-200 text-green-500",
 			heading: "Get a personalized bot invite link",
 			content:
 				"When your application is approved, you will be given a Discord bot invite link that is customized for your server. You can setup all of the bot except the Twitter Raid features which requires the Twitter API 'elevated' access application. The process of applying for the Twitter API can take up to 72 hours.",
 		},
 		{
-			color: "bg-indigo-50 ring-indigo-200",
+			color: "bg-indigo-50 ring-indigo-200 text-indigo-500",
 			heading: "Start setting up the Discord bot",
 			content:
 				"After the bot has been invited to your server you can follow our getting started documentation here. Additionally we have a built-in quick setup command that will get you started.",
@@ -74,11 +74,13 @@ const List = () => {
 			{list.map((item, idx) => {
 				return (
 					<li key={idx} className="flex items-center">
-						<span className="relative ml-4 grid">
+						<span className="relative ml-4 grid bg-orange-50 ring-orange-200">
 							<div
 								className={`absolute h-12 rotate-45 place-self-center rounded-full py-8 px-5 ring-1 ${item.color}`}
 							></div>
-							<div className="absolute place-self-center">
+							<div
+								className={`absolute place-self-center ${item.color}`}
+							>
 								{idx + 1}
 							</div>
 						</span>
